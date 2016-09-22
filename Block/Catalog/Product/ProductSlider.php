c9bd9de3abb9060d3bb8a9e282de2b9b53a3ddf1
Alex Wang <selltrends@gmail.com>
2016-09-22 14:16:05 +0800

    add productslider block class & template

---
+++ Block/Catalog/Product/ProductSlider.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Copyright © 2016 Atopt Limited. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+/**
+ * Product gallery
+ *
+ * @author      Magento Core Team <core@magentocommerce.com>
+ */
+namespace Atopt\Slider\Block\Catalog\Product;
+
+class ProductSlider extends \Magento\Catalog\Block\Product\ListProduct
+{
+
+}
